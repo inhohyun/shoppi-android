397c4cee8ec9eca25383477764912c8d4598894f
인호현 <96523102+inhohyun@users.noreply.github.com>
2022-12-28 18:20:17 +0900

    세션7 레이아웃 구현 실습

---
+++ app/src/main/java/com/shoppi/shoppi/app/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.shoppi.shoppi.app
+
+import com.bumptech.glide.module.AppGlideModule
+
+@com.bumptech.glide.annotation.GlideModule
+class GlideModule: AppGlideModule(){
+}
--- app/src/main/java/com/shoppi/shoppi/app/HoemFragment.kt
+++ app/src/main/java/com/shoppi/shoppi/app/HoemFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.json.JSONObject
@@ -23,20 +25,9 @@ class HoemFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_detail)
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
 
-            //context가 nullable일 수 있기 때문에 ?와 함께 사용
-            context?.assets?.open("home.json")?.use{ inputStream ->
-                val size = inputStream.available()
-                val bytes = ByteArray(size)
-                inputStream.read(bytes)
-                val homeData = String(bytes)
-                Log.d("homeData",homeData)
-            }
-
-        }
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")//null일경우 빈문자 출력
@@ -45,9 +36,11 @@ class HoemFragment: Fragment() {
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
-            val titleValue = Title(text, iconUrl)
-            titleValue.text
 
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
 //            val topBanners = jsonObject.getJSONArray("top_banners")
 //            val firstBanner = topBanners.getJSONObject(0)
 //            val label = firstBanner.getString("label")
